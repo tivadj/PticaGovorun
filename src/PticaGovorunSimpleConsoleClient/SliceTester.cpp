0d155842650770b376b0ae26b68101a9307795a2
tivadj <tivadj@gmail.com>
2016-09-15 16:02:19 +0300

    refactor: cleaned unused dependencies (arv::array_view)

--- src/PticaGovorunSimpleConsoleClient/SliceTester.cpp
+++ src/PticaGovorunSimpleConsoleClient/SliceTester.cpp
@@ -1,39 +1,9 @@
 #include "stdafx.h"
 #include <iostream>
-#if HAS_ARV_IMPL
-#include "array_view.hpp"
-#endif
 #include "ClnUtils.h"
 
 namespace SliceTesterNS
 {
-
-#if HAS_ARV_IMPL
-	void testArvImpl()
-	{
-		std::vector<float> v1 = { 1, 2, 3 };
-		arv::array_view<float> av1 = arv::make_view(std::begin(v1), std::end(v1));
-		for (auto& f : av1)
-		{
-			std::cout << f << std::endl;
-		}
-
-		float* pData = &v1[0];
-		arv::array_view<float> sPointer = arv::make_view(pData, 3);
-		for (auto& f : sPointer)
-		{
-			std::cout << f << std::endl;
-		}
-
-		std::vector<float> vEmpty = {};
-		arv::array_view<float> av2 = arv::make_view(vEmpty);
-		for (auto& f : av2)
-		{
-			std::cout << f << std::endl;
-		}
-	}
-#endif
-
 	void testSliceImpl()
 	{
 		std::vector<float> v1 = { 1, 2, 3 };
